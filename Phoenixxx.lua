4ca370d19127910540c7183b9f64a82a0262109d
Vildd <40041157+Vildd@users.noreply.github.com>
2018-06-08 10:00:51 +0500

    Update Phoenixxx.lua

--- Phoenixxx.lua
+++ Phoenixxx.lua
@@ -4,17 +4,20 @@ Phoenix.optionEnable = Menu.AddOptionBool({ "Hero Specific", "Phoenix" }, "Phoen
 Phoenix.FireSpiritKey = Menu.AddKeyOption({ "Hero Specific", "Phoenix" }, "Fire spirit key", Enum.ButtonCode.BUTTON_CODE_NONE)
 
 function Phoenix.OnUpdate()
-  if not Menu.IsEnabled( Phoenix.optionEnable ) then return end
+  if not Menu.IsEnabled( Phoenix.optionEnable ) then return 
+  end
 
   me = Heroes.GetLocal()
   mana = NPC.GetMana(me)
   
-  if not me or NPC.GetUnitName(me) ~= "npc_dota_hero_phoenix" then return end
+  if not me or NPC.GetUnitName(me) ~= "npc_dota_hero_phoenix" then return 
+  end
 
-    if Menu.IsKeyDownOnce(Phoenix.FireSpiritKey) then Phoenix.FireSpirit(me, enemy) end
+    if Menu.IsKeyDownOnce(Phoenix.FireSpiritKey) then 
+	Phoenix.FireSpirit(me, enemy) 
+	end
 	
 	enemy = Input.GetNearestHeroToCursor(Entity.GetTeamNum(me), Enum.TeamType.TEAM_ENEMY)
-
 end
 
 function Phoenix.InFront(delay)
@@ -37,14 +40,30 @@ id = NPC.GetAbility(me, "phoenix_icarus_dive")
 fs = NPC.GetAbility(me, "phoenix_fire_spirits")
 lfs = NPC.GetAbility(me, "phoenix_launch_fire_spirit")
 sn = NPC.GetAbility(me, "phoenix_supernova")
-	  
+
+if Ability.IsCastable(sn, mana) and Ability.IsReady(sn) then
+if Ability.IsCastable(fs, mana-Ability.GetManaCost(sn)) and Ability.IsReady(fs) then
 	  Ability.CastNoTarget(fs)
-      
-		if NPC.HasState(enemy, Enum.ModifierState.MODIFIER_STATE_ROOTED) or NPC.HasState(enemy, Enum.ModifierState.MODIFIER_STATE_STUNNED) then
+	  end
+else
+if Ability.IsCastable(fs, mana) and Ability.IsReady(fs) then
+	  Ability.CastNoTarget(fs)
+	  end
+	  end
+	
+	if not NPC.HasModifier(enemy, "modifier_phoenix_fire_spirit_burn") then burn = false
+	end
+	
+      if Ability.IsCastable(lfs, 0) and Ability.IsReady(lfs) then 
+	  if NPC.HasModifier(enemy, "modifier_phoenix_fire_spirit_burn") and burn == true then return 
+	  else
+	  burn = true 
+		if NPC.HasState(enemy, Enum.ModifierState.MODIFIER_STATE_ROOTED) or NPC.HasState(enemy, Enum.ModifierState.MODIFIER_STATE_STUNNED) or not NPC.IsRunning(enemy) then
 			Ability.CastPosition(lfs, Phoenix.InFront(200))
-			elseif NPC.IsRunning(enemy) then
+			else
 			Ability.CastPosition(lfs, Phoenix.InFront(650))
-			else Ability.CastPosition(lfs, Phoenix.InFront(200))
+		end
+		end
 		end
 end
 
